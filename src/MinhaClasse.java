c070c95edf78b8eca99f8cafcd9714a596fc3942
caahcavalcantte <cacavalcantte@gmail.com>
2023-08-23 14:56:59 -0300

    Adicionados comentários ao código de inicialização da classe MinhaClasse

--- src/MinhaClasse.java
+++ src/MinhaClasse.java
@@ -9,5 +9,18 @@ public class MinhaClasse {
 
    // Imprime a mensagem de boas-vindas no console
     System.out.print("Olá turma, sejam bem-vindo!");
+    //Declaração da variável 'primeiroNome' e atribuição de valor a ela
+    String primeiroNome = "Cássia";
+     //Declaração da variável 'segundoNomeNome' e atribuição de valor a ela
+    String segundoNome = "Cavalcante";
+    // Declaração de variável para armazenar o nome completo
+    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+    // Imprime nome completo no console
+    System.out.println(nomeCompleto);
+ }
+ // Esta é uma função que recebe dois nomes (primeiroNome e segundoNome) como entrada
+ // e retorna o nome completo formado pela concanetação desses nome com espaço entre eles.
+ public static String nomeCompleto (String primeiroNome, String segundoNome ) {
+    return primeiroNome.concat(" ").concat(segundoNome);
  }
 }
