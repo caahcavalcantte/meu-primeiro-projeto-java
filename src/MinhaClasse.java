cc1dc48b46fa47f29f0ff7a44cc1aaa43a522bec
caahcavalcantte <cacavalcantte@gmail.com>
2023-08-23 14:19:57 -0300

    comentando o código

--- src/MinhaClasse.java
+++ src/MinhaClasse.java
@@ -1,7 +1,13 @@
 public class MinhaClasse {
-  
+  /** Esta é a classe Minha Classe.
+   * Ela faz coisas incríveis.
+   */
  public static void main(String[] args) {
+  /** Este é o método principal (main) do programa
+   * É o ponto de entrada do programa
+   */
+
+   // Imprime a mensagem de boas-vindas no console
     System.out.print("Olá turma, sejam bem-vindo!");
  }
-
 }
