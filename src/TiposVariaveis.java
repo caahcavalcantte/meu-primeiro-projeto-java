bedbf8cb24190a5a8c4b83aa0eb96d0791f06302
caahcavalcantte <cacavalcantte@gmail.com>
2023-08-27 20:46:57 -0300

    decrementar valores em um

--- src/TiposVariaveis.java
+++ src/TiposVariaveis.java
@@ -3,7 +3,7 @@ public class TiposVariaveis {
   public static void main(String[] args) {
    int numero = 5;
 
-   System.out.println(++ numero);
+   System.out.println(numero --);
 
    System.out.print(numero);
   }
