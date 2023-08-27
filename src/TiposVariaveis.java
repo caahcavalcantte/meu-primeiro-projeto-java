4b98f6f2e3c428422e485cf296fe6cfecec1ab1d
caahcavalcantte <cacavalcantte@gmail.com>
2023-08-27 20:43:19 -0300

    imprimindo  primeiro valor e incrementando mais um

--- src/TiposVariaveis.java
+++ src/TiposVariaveis.java
@@ -2,8 +2,9 @@ public class TiposVariaveis {
 
   public static void main(String[] args) {
    int numero = 5;
-   numero = numero + 1;
 
-   System.out.println(numero);
+   System.out.println(numero++);
+
+   System.out.print(numero);
   }
 }
