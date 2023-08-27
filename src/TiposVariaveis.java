b0017b848a962e106fb2da27773d9bf797ed2993
caahcavalcantte <cacavalcantte@gmail.com>
2023-08-27 20:08:30 -0300

    short, int, double

--- src/TiposVariaveis.java
+++ src/TiposVariaveis.java
@@ -1,3 +1,15 @@
 public class TiposVariaveis {
-  
+
+  public static void main(string[] args) throws Exception {
+    double salarioMinino = 2500;
+
+    short numeroCurto = 1;
+    int numeroNormal = numeroCurto;
+    short numeroCurto2 = (short) numeroNormal;
+
+    int numero = 5;
+    numero = 10;
+
+    System.out.print(numero);
+  }
 }
