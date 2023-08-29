85f7917c7a01a72b8dcef3bb17a499bb6435182e
caahcavalcantte <cacavalcantte@gmail.com>
2023-08-29 10:37:34 -0300

    operadores E e OU &, ||

--- src/Operadores.java
+++ src/Operadores.java
@@ -1,6 +1,6 @@
 public class Operadores {
 
-  public static void main(String[] args) {
+  /* public static void main(String[] args) {
     String nomeUm = "Cássia";
     String nomeDois = "Cavalcantte";
 
@@ -18,5 +18,39 @@ public class Operadores {
 
     System.out.println("numeroUm é maior a numeroDois?" + simNao);
 
-  }
+  } */
+  public static void main(String[] args) {
+    boolean condicao1=true;
+    boolean condicao2=false;
+
+    if(condicao1 && (7 > 4)){
+      System.out.println("as duas condições são verdadeiras");
+    }
+
+    if(condicao1 || condicao2){
+      System.out.println("as duas condições são verdadeiras");
+    }
+
+    System.out.println("Fim");
+
+
+
+    /*String nomeUm = "Cássia";
+    String nomeDois = "Cavalcantte";
+
+    System.out.println(nomeUm.equals(nomeDois));
+
+
+
+    int numero1 = 1;
+    int numero2 = 2;
+
+    boolean simNao = numero1 > numero2;
+    if(numero1 > numero2){
+      System.out.println("a nossa condição é verdadeira");
+    }
+
+    System.out.println("numeroUm é maior a numeroDois?" + simNao);*/
+
+  } 
 }
