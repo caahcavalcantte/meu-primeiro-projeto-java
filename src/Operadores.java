0ebd48785d53756cc792e27a7b5cfc767584a8e8
caahcavalcantte <cacavalcantte@gmail.com>
2023-08-29 10:24:55 -0300

    operadores relacionais para numero, e .equals obje

--- src/Operadores.java
+++ src/Operadores.java
@@ -1,10 +1,20 @@
 public class Operadores {
 
   public static void main(String[] args) {
+    String nomeUm = "Cássia";
+    String nomeDois = "Cavalcantte";
+
+    System.out.println(nomeUm.equals(nomeDois));
+
+
+
     int numero1 = 1;
     int numero2 = 2;
 
     boolean simNao = numero1 > numero2;
+    if(numero1 > numero2){
+      System.out.println("a nossa condição é verdadeira");
+    }
 
     System.out.println("numeroUm é maior a numeroDois?" + simNao);
 
